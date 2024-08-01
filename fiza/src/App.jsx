3409787c309aaddda22ea17d7ca4b2fe954d403d
adon <adonxx875@gmail.com>
2024-08-01 18:31:13 +0600

    initial

---
+++ fiza/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Tictactoe from './components/Tictactoe/Tictactoe'
+
+function App() {
+  
+
+  return (
+    <>
+    <Tictactoe/>
+     
+    </>
+  )
+}
+
+export default App
